5c9d6b56e5843582ee1d409b0ef12b234344a34c
asimbinahammed <asim242370@gmail.com>
2021-10-11 08:19:21 +0530

    Welcome message

---
+++ src/main/java/com/bridgelab/AddressBook.java
@@ -0,0 +1,14 @@
+/*
+@author- ASIM AHAMMED
+ */
+
+package com.bridgelab;
+
+/*
+main class
+ */
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+    }
+}
